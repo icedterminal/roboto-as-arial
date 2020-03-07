c4bd46020a748c60aae11fc953349e793f96aeb8
icedterminal <icedterminal@gmail.com>
2020-03-06 21:12:44 -0800

    Upload all the files!

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# roboto-as-arial
-Dislike Arial? Replace it with Roboto. (Windows only)
+# Roboto as Arial
+
+I loathe Arial with an unbridled passion. I can not even begin to describe my [reprehensible disgust](https://www.google.com/search?q=arial+is+bad) for this forgery of a font.
+
+Replace it with Roboto.
+
+## Undo, Revert, Restore
+
+Before you attempt this, create a restore point. Though nothing has happened to me on any of my VMs or the host system, you never know. Plus if you want the original Arial back, restore is easy. Even if you don't, the script makes a backup copy of the original ArialMT fonts and copies them to your documents folder.
+
+## How To
+
+There are two ways: Run a script, or do it all manually.
+
+The PowerShell script removes the fonts themselves, registry links, the Arial SxS directory and the manifest. If you don't remove the manifest and the SxS store, then Windows will restore the original Arial fonts during a system clean up. Don't fret, because DISM will pass a clean up if the files aren't counted. Because they're not there!
+
+If you want to use the script, follow the directions below:
+
+1. Open PowerShell as admin (Win + X, A)
+2. Type or copy/paste in: `set-executionpolicy unrestricted`
+    - If you don't do this, you can't run the script which makes this job ten times easier.
+3. Clone/Download/Save this repo.
+4. Extract the contents to your desktop.
+
+![desktop](https://i.imgur.com/efgsOKH.png)
+
+5. Run the script. Type or copy/paste in: `$env:USERPROFILE\Desktop\roboto-as-arial.ps1`
+6. When the script completes, reboot.
+    - If you encounter any errors please open an issue.
+7. Optional: You can restore or revert the policy of PowerShell scripts with `set-executionpolicy restricted`
+
+## Well... shucks
+
+Updates have a chance to undo all of this. If you notice it, repeat the how-to. OS upgrades (eg: 1809 to 1903) will restore good 'ol ugly Arial.
+
+## Notes
+
+I purposefully made the script explicit. One by one. I could have condensed it down as others would call it "more efficient", but elected not to. I realise some people might not fully understand what the script does. And laying it all out makes it more understandable. So you see every action it makes as you read over it. Feel free to scrutinise it if you have any doubts.
+
+I tested this script multiple times over with different versions of Windows 10 in VM.
+
+# Legal stuff
+
+If you use the script, do keep in mind you are using at your own risk.
+
+__Disclaimer of Warranty__
+
+Unless required by applicable law or agreed to in writing, Licensor provides the Work (and each Contributor provides its Contributions) on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Work and assume any risks associated with Your exercise of permissions under this License.
+
+__Limitation of Liability__
+
+In no event and under no legal theory, whether in tort (including negligence), contract, or otherwise, unless required by applicable law (such as deliberate and grossly negligent acts) or agreed to in writing, shall any Contributor be liable to You for damages, including any direct, indirect, special, incidental, or consequential damages of any character arising as a result of this License or out of the use or inability to use the Work (including but not limited to damages for loss of goodwill, work stoppage, computer failure or malfunction, or any and all other commercial damages or losses), even if such Contributor has been advised of the possibility of such damages.
+
+---
+
+Roboto by Christian Robertson is licensed under the Apache License 2.0.
+
+To keep with consistency, the rest of this projects files are also licensed under the Apache License 2.0.
+
---
+++ roboto-as-arial.ps1
@@ -0,0 +1,160 @@
+﻿Write-Host "Roboto-as-Arial is a free and open source script that replaces the system font Arial with Roboto.
+
+Roboto by Christian Robertson is licensed under the Apache License 2.0.
+Roboto-as-Arial project is licensed under the Apache License 2.0.
+If you use the script, do keep in mind you are using at your own risk!
+A backup of the original ArialMT fonts are stored in $env:USERPROFILE\Documents\ArialMT_Backup
+
+Please close all other running applications as this will prohibit the deletion of Arial!"
+
+$title    = 'You must accept the terms and license to continue.'
+$question = 'Do you agree?'
+$choices  = '&Yes', '&No'
+
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    Write-Host 'confirmed'
+} else {
+    Write-Host 'cancelled'
+}
+Write-Host "======================== Delete reg entries..." -ForegroundColor Yellow
+# We have to delete the existing fonts from the system registry.
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial (TrueType)"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Bold (TrueType)"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Bold Italic (TrueType)"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Italic (TrueType)"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Black (TrueType)"
+Write-Host "======================== Permissions..." -ForegroundColor Yellow
+cd "$env:WINDIR\Fonts"
+# take ownership
+takeown /f arial.ttf /a
+takeown /f arialbd.ttf /a
+takeown /f arialbi.ttf /a
+takeown /f ariali.ttf /a
+takeown /f ariblk.ttf /a
+# strip all permissions
+icacls arial.ttf /inheritance:r
+icacls arialbd.ttf /inheritance:r
+icacls arialbi.ttf /inheritance:r
+icacls ariali.ttf /inheritance:r
+icacls ariblk.ttf /inheritance:r
+# grant all full permissions
+icacls arial.ttf /t /grant Administrators:F
+icacls arial.ttf /t /grant Users:F
+icacls arial.ttf /t /grant SYSTEM:F
+icacls arial.ttf /t /grant Everyone:F
+icacls arial.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls arialbd.ttf /t /grant Administrators:F
+icacls arialbd.ttf /t /grant Users:F
+icacls arialbd.ttf /t /grant SYSTEM:F
+icacls arialbd.ttf /t /grant Everyone:F
+icacls arialbd.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls arialbi.ttf /t /grant Administrators:F
+icacls arialbi.ttf /t /grant Users:F
+icacls arialbi.ttf /t /grant SYSTEM:F
+icacls arialbi.ttf /t /grant Everyone:F
+icacls arialbi.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls ariali.ttf /t /grant Administrators:F
+icacls ariali.ttf /t /grant Users:F
+icacls ariali.ttf /t /grant SYSTEM:F
+icacls ariali.ttf /t /grant Everyone:F
+icacls ariali.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls ariblk.ttf /t /grant Administrators:F
+icacls ariblk.ttf /t /grant Users:F
+icacls ariblk.ttf /t /grant SYSTEM:F
+icacls ariblk.ttf /t /grant Everyone:F
+icacls ariblk.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+Write-Host "======================== Backup fonts..." -ForegroundColor Yellow
+# we must backup the original arial family just in case.
+New-Item -Path "$env:USERPROFILE\Documents" -Name "ArialMT_Backup" -ItemType "directory"
+Start-Sleep -s 2 > $null
+Copy-Item "$env:WINDIR\Fonts\ari*.ttf" -Destination "$env:USERPROFILE\Documents\ArialMT_Backup"
+Start-Sleep -s 2 > $null
+Write-Host "======================== Delete fonts..." -ForegroundColor Yellow
+# goodbye!
+Remove-Item -Path $env:WINDIR\Fonts\arial.ttf -Force
+Remove-Item -Path $env:WINDIR\Fonts\arialbd.ttf -Force
+Remove-Item -Path $env:WINDIR\Fonts\arialbi.ttf -Force
+Remove-Item -Path $env:WINDIR\Fonts\ariali.ttf -Force
+Remove-Item -Path $env:WINDIR\Fonts\ariblk.ttf -Force
+Write-Host "======================== Permissions..." -ForegroundColor Yellow
+cd "$env:WINDIR\WinSxS"
+# again with the permissions but this time with the SxS store.
+takeown /f amd64_microsoft-windows-font-truetype-arial* /r /a
+takeown /f amd64_microsoft-windows-f..truetype-arialblack* /r /a
+icacls amd64_microsoft-windows-font-truetype-arial* /inheritance:r
+icacls amd64_microsoft-windows-f..truetype-arialblack* /inheritance:r
+icacls amd64_microsoft-windows-font-truetype-arial* /t /grant Administrators:F
+icacls amd64_microsoft-windows-font-truetype-arial* /t /grant Users:F
+icacls amd64_microsoft-windows-font-truetype-arial* /t /grant SYSTEM:F
+icacls amd64_microsoft-windows-font-truetype-arial* /t /grant Everyone:F
+icacls amd64_microsoft-windows-font-truetype-arial* /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls amd64_microsoft-windows-f..truetype-arialblack* /t /grant Administrators:F
+icacls amd64_microsoft-windows-f..truetype-arialblack* /t /grant Users:F
+icacls amd64_microsoft-windows-f..truetype-arialblack* /t /grant SYSTEM:F
+icacls amd64_microsoft-windows-f..truetype-arialblack* /t /grant Everyone:F
+icacls amd64_microsoft-windows-f..truetype-arialblack* /t /grant "NT SERVICE\TrustedInstaller:F"
+cd "$env:WINDIR\WinSxS\Manifests"
+takeown /f amd64_microsoft-windows-font-truetype-arial*.manifest /a
+takeown /f amd64_microsoft-windows-f..truetype-arialblack*.manifest /a
+icacls amd64_microsoft-windows-font-truetype-arial*.manifest /inheritance:r
+icacls amd64_microsoft-windows-f..truetype-arialblack*.manifest /inheritance:r
+icacls amd64_microsoft-windows-font-truetype-arial*.manifest /t /grant Administrators:F
+icacls amd64_microsoft-windows-font-truetype-arial*.manifest /t /grant Users:F
+icacls amd64_microsoft-windows-font-truetype-arial*.manifest /t /grant SYSTEM:F
+icacls amd64_microsoft-windows-font-truetype-arial*.manifest /t /grant Everyone:F
+icacls amd64_microsoft-windows-font-truetype-arial*.manifest /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls amd64_microsoft-windows-f..truetype-arialblack*.manifest /t /grant Administrators:F
+icacls amd64_microsoft-windows-f..truetype-arialblack*.manifest /t /grant Users:F
+icacls amd64_microsoft-windows-f..truetype-arialblack*.manifest /t /grant SYSTEM:F
+icacls amd64_microsoft-windows-f..truetype-arialblack*.manifest /t /grant Everyone:F
+icacls amd64_microsoft-windows-f..truetype-arialblack*.manifest /t /grant "NT SERVICE\TrustedInstaller:F"
+Write-Host "======================== Delete SxS..." -ForegroundColor Yellow
+# goodbye!
+cd "$env:WINDIR\WinSxS"
+Remove-Item amd64_microsoft-windows-font-truetype-arial*.manifest -Force
+Remove-Item amd64_microsoft-windows-font-truetype-arial* -Recurse -Force
+Remove-Item amd64_microsoft-windows-f..truetype-arialblack*.manifest -Force
+Remove-Item amd64_microsoft-windows-f..truetype-arialblack* -Recurse -Force
+cd "$env:WINDIR\WinSxS\Manifests"
+Remove-Item amd64_microsoft-windows-font-truetype-arial*.manifest -Force
+Remove-Item amd64_microsoft-windows-f..truetype-arialblack*.manifest -Force
+Write-Host "======================== Copy replacement fonts..." -ForegroundColor Yellow
+# now we copy over the new fonts.
+cd "$env:WINDIR\Fonts"
+Copy-Item "$env:USERPROFILE\Desktop\roboto-as-arial\*" -Destination "$env:WINDIR\Fonts" -Recurse
+Write-Host "======================== Permissions..." -ForegroundColor Yellow
+icacls arial.ttf /t /grant Administrators:F
+icacls arial.ttf /t /grant Users:F
+icacls arial.ttf /t /grant SYSTEM:F
+icacls arial.ttf /t /grant Everyone:F
+icacls arial.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls arialbd.ttf /t /grant Administrators:F
+icacls arialbd.ttf /t /grant Users:F
+icacls arialbd.ttf /t /grant SYSTEM:F
+icacls arialbd.ttf /t /grant Everyone:F
+icacls arialbd.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls arialbi.ttf /t /grant Administrators:F
+icacls arialbi.ttf /t /grant Users:F
+icacls arialbi.ttf /t /grant SYSTEM:F
+icacls arialbi.ttf /t /grant Everyone:F
+icacls arialbi.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls ariali.ttf /t /grant Administrators:F
+icacls ariali.ttf /t /grant Users:F
+icacls ariali.ttf /t /grant SYSTEM:F
+icacls ariali.ttf /t /grant Everyone:F
+icacls ariali.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+icacls ariblk.ttf /t /grant Administrators:F
+icacls ariblk.ttf /t /grant Users:F
+icacls ariblk.ttf /t /grant SYSTEM:F
+icacls ariblk.ttf /t /grant Everyone:F
+icacls ariblk.ttf /t /grant "NT SERVICE\TrustedInstaller:F"
+Write-Host "======================== Write reg entries..." -ForegroundColor Yellow
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial (TrueType)" -Value "arial.ttf"  -PropertyType "String"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Bold (TrueType)" -Value "arialbd.ttf"  -PropertyType "String"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Bold Italic (TrueType)" -Value "arialbi.ttf"  -PropertyType "String"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Italic (TrueType)" -Value "ariali.ttf"  -PropertyType "String"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Arial Black (TrueType)" -Value "ariblk.ttf"  -PropertyType "String"
+Write-Host "======================== It is done." -ForegroundColor Yellow
+Write-Host "PLEASE REBOOT! The fonts are registered but the font cache has to rebuild on reboot." -ForegroundColor Red -BackgroundColor White
+cd "$env:WINDIR\system32"
